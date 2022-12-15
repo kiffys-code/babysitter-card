225391ff34b6d52469aef19a458efcb994d3e59c
Kiffy <106380921+kiffys-code@users.noreply.github.com>
2022-12-15 08:39:05 +0100

    fix: last commit didn't include all changes

---
+++ src/components/ConsentView/ConsentList.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import ConsentListItem from "components/ConsentView/ConsentListItem";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1rem 0.5rem;
+`
+
+const NewConsentsMessage = styled.p`
+    text-align: center;
+    font-style: italic;
+    font-size: 1rem;
+`
+
+const ConsentList = ({consents}) => {
+
+    const renderedConsents = consents && consents.length > 0 ? consents.map((item, index) => 
+        <ConsentListItem 
+            consent={item}
+            key={index}
+        />
+    ) : <NewConsentsMessage>Click the Edit Icon to Begin!</NewConsentsMessage>;
+
+    return (
+        <Container>
+            {renderedConsents}
+        </Container>
+    );
+}
+
+export default ConsentList;
